9f8f2c8d6b38b54304bb4d13324321e1acb78b82
HPC-ChristopherRS <20222@haurakiplains.school.nz>
2024-08-20 11:34:06 +1200

    20/08/24 Bullets work as intended.

--- bullet.py
+++ bullet.py
@@ -4,25 +4,39 @@ from settings import *
 
 class Bullet:
     def __init__(self, x, y):
-        self.pos = (x,y)
+        self.pos = (x, y)
         mx, my = pygame.mouse.get_pos()
         self.dir = (mx - x, my - y)
         length = math.hypot(*self.dir)
-        if length == 0:
-            self.dir = (0,-1)
+        if length == 0.0:
+            self.dir = (0, -1)
         else:
-            self.dir(self.dir[0]/length,self.dir[1]/length)
+            self.dir = (self.dir[0]/length, self.dir[1]/length)
         angle = math.degrees(math.atan2(-self.dir[1], self.dir[0]))
-        
-        self.bullet = pygame.Surface((7,2)).convert_alpha
-        self.bullet.fill = ((255,255,255))
-        self.bullet = pygame.transform.rotate(self.bullet, angle)
-        self.speed = 2
 
-    def update(self):
-        self.pos = (self.pos[0]+self.dir[0]*self.speed,
-                    self.pos[1]+self.dir[1]*self.speed)    
+        self.bullet = pygame.Surface((10, 10)).convert_alpha()
+        self.bullet.fill((0, 0, 0))
+        self.bullet = pygame.transform.rotate(self.bullet, angle)
+        self.speed = 5
+
+    def update(self):  
+        self.pos = (self.pos[0]+self.dir[0]*self.speed, 
+                    self.pos[1]+self.dir[1]*self.speed)
 
     def draw(self, surf):
-        self.pos = (self.bullet.get_rect(center = self.pos))
-        surf.blit(self.bullet, bullet_rect)
+        bullet_rect = self.bullet.get_rect(center = self.pos)
+        surf.blit(self.bullet, bullet_rect)  
+
+    def check_collision(self, rect=None):
+        if rect is None:
+            rect = self.rect
+        for row in range(len(self.level.grid)):
+            for column in range(len(self.level.grid[0])):
+                if self.level.grid[row][column] == 1:
+                    wall_rect = pygame.Rect(
+                        (MARGIN + WIDTH) * column + MARGIN, 
+                        (MARGIN + HEIGHT) * row + MARGIN, 
+                        WIDTH, HEIGHT)
+                    
+                    if rect.colliderect(wall_rect):
+                        print("Noah is gay")
--- settings.py
+++ settings.py
@@ -7,5 +7,5 @@ WIDTH, HEIGHT, MARGIN = 32, 32, 1
 COOLDOWN_TIME = 2000
 DASH_DISTANCE = 80
 speed = 2.5
-enemy_speed = 5
+enemy_speed = 3
 screen = pygame.display.set_mode((660, 660))
